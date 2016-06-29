64e19a2ff0709a54955433c813472a3802daf7db
Henry Osadzinski <henry@osadzinski.co.uk>
2016-06-29 10:37:15 +0100

    store setup and components defined

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -4,12 +4,34 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 // the initial state object
-const state = {};
+const state = {
+  bernards: []
+};
 
 // All possible mutations (actions) that can be applied to the state.
 // Not the best terminology considering we're striving for immutability
 // but it's what the official docs call them!
-const mutations = {};
+const mutations = {
+
+  GET_BERNARDS(state, results) {
+    state.bernards.push(results);
+  },
+
+  VOTE_BERNARD(state, bernard) {
+    bernard.votes += 1;
+  },
+
+  WIPE_BERNARDS(state) {
+    state.bernards = [];
+  },
+
+  BEST_BERNARD(state) {
+    state.bernards.sort(function(a, b) {
+      return b.votes - a.votes;
+    });
+    return state.bernards[0];
+  }
+};
 
 export default new Vuex.Store({
   state,
