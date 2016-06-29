0241e5b536bd3104cd3748a35450579845ea9f5e
Henry Osadzinski <henry@osadzinski.co.uk>
2016-06-29 11:57:01 +0100

    added action method to vote page

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,3 +1,7 @@
 // export const actionName = ({ dispatch }) => {
 //   dispatch('ACTION_NAME');
 // }
+
+export const getBernards = ({ dispatch }, results) => {
+  dispatch('GET_BERNARDS', results);
+}
