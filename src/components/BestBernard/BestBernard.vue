0e11498040f154e4d6ea77d5bcae7c2829d4b9e9
Henry Osadzinski <henry@osadzinski.co.uk>
2016-06-29 16:07:23 +0100

    IT LIVES

--- src/components/BestBernard/BestBernard.vue
+++ src/components/BestBernard/BestBernard.vue
@@ -1,3 +1,19 @@
 <template>
-    <h3>BERNARD HILL</h3>
+    <div>
+        <h3>BESTEST BERNARD HILL</h3>
+        <img v-bind:src="bestBernard.contentUrl">
+    </div>
 </template>
+
+<script>
+
+    import { getBestBernard } from '../../vuex/actions';
+
+    export default {
+        vuex: {
+            getters: {
+                bestBernard: state => state.bestBernard
+            }
+        }
+    }
+</script>
